e9d15da450450bb165e8177cb8e2d8e2e16f654b
twizzworld <132231765+twizzworld@users.noreply.github.com>
2025-01-24 17:55:34 -0600

    removed rrs link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,17 +19,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 "
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
+
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 "
